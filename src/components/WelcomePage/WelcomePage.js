5fa53e7b0bc6ce06a49ca19beda13423146fc68c
Ghjattu <icracked369@gmail.com>
2022-07-29 08:36:14 +0800

    修改下滑按钮的点击处理逻辑

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -3,16 +3,24 @@ import Typewriter from "../Typewriter/Typewriter";
 import './WelcomePage.css';
 
 const WelcomePage = (props) => {
+    const handleClick = (e) => {
+        const welcomePage = document.querySelector("#welcome-page");
+        if (window.scrollTo) {
+            e.preventDefault();
+            window.scrollTo({"behavior": "smooth", "top": welcomePage.offsetHeight});
+        }
+    };
+
     return (
-        <div className="welcome-page">
+        <div className="welcome-page" id="welcome-page">
             <h2 className="website-title">{props.websiteTitle}</h2>
             <Typewriter strings={[
                 '悟已往之不谏，知来者之可追。',
                 '祝你好运，也祝我好运。',
             ]}/>
-            <a className="scroll-down" href="#article-card-list-wrapper">
+            <button className="scroll-down" onClick={handleClick} onKeyDown={handleClick}>
                 <div className="arrow"></div>
-            </a>
+            </button>
         </div>
     );
 };
