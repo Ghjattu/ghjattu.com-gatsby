76bb49a598114eef6833632f6b754204cdb3a915
Ghjattu <icracked369@gmail.com>
2022-08-20 14:43:22 +0800

    更新页脚图标

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,27 @@
 import React from "react";
-import {Icon} from '@iconify/react';
+import { Icon } from '@iconify/react';
 import './Footer.css';
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="technology">
-                <nav>
-                    <span>powered by</span>
-                    <section className="tech-links">
-                        <a href="https://reactjs.org/" target="_blank" title="React" rel="noopener noreferrer">
-                            <span>React</span>
-                            <Icon icon="logos:react" width="20" height="20" inline={true}/>
-                        </a>
-                        <a href="https://www.gatsbyjs.com/" target="_blank" title="Gatsby" rel="noopener noreferrer">
-                            <span>Gatsby</span>
-                            <Icon icon="logos:gatsby" width="20" height="20" inline={true}/>
-                        </a>
-                    </section>
-                </nav>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className="footer">
+			<div className="technology">
+				<nav>
+					<span>powered by</span>
+					<section className="tech-links">
+						<a href="https://www.gatsbyjs.com/" target="_blank" title="Gatsby" rel="noopener noreferrer">
+							<span>Gatsby</span>
+							<Icon icon="logos:gatsby" width="20" height="20" inline={true}/>
+						</a>
+						<a href="https://www.mongodb.com/" target="_blank" title="MongoDB" rel="noopener noreferrer">
+							<span>MongoDB</span>
+							<Icon icon="logos:mongodb-icon" width="20" height="20" inline={true}/>
+						</a>
+					</section>
+				</nav>
+			</div>
+		</footer>
+	)
 };
 
 export default Footer
